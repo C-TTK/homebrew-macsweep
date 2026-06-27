ecb3c80f14ffa515c70ae3186496478493ae4139
ciyo65 <cihatyerli@gmail.com>
2026-06-27 08:43:22 +0200

    macsweep 2.0.8 (was stale at 2.0.6; v2.0.7 never bumped the tap)

--- Formula/macsweep.rb
+++ Formula/macsweep.rb
@@ -1,9 +1,9 @@
 class Macsweep < Formula
   desc "Safe macOS disk cleanup CLI companion for MacSweep"
   homepage "https://macsweep.app"
-  url "https://releases.macsweep.app/macsweep-2.0.6.tar.gz"
-  version "2.0.6"
-  sha256 "9ceb6a08b1d33f3f92c71775ad08f7626abcdd6f9f258bfed310e213a23f3194"
+  url "https://releases.macsweep.app/macsweep-2.0.8.tar.gz"
+  version "2.0.8"
+  sha256 "487a3691f89dc09ab9528f74d1c673f820ea3152986e88ae7484f29480e6c188"
   license "Proprietary"
 
   depends_on macos: :ventura
