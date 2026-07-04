b2f60df8a158c388db294340ed420f084a659533
ciyo65 <cihatyerli@gmail.com>
2026-07-04 20:08:17 +0200

    macsweep 2.1.2

--- Formula/macsweep.rb
+++ Formula/macsweep.rb
@@ -1,9 +1,9 @@
 class Macsweep < Formula
   desc "Safe macOS disk cleanup CLI companion for MacSweep"
   homepage "https://macsweep.app"
-  url "https://releases.macsweep.app/macsweep-2.1.1.tar.gz"
-  version "2.1.1"
-  sha256 "d9c4de5dcf19d0766bf5d0d8eb1e81d2a3c37185e6b231a85e499a1b8aeae7ce"
+  url "https://releases.macsweep.app/macsweep-2.1.2.tar.gz"
+  version "2.1.2"
+  sha256 "6bc14dadd5813545b6e174606e49c5f5bb828139581976b4180d68851b415cfe"
   license "Proprietary"
 
   depends_on macos: :ventura
